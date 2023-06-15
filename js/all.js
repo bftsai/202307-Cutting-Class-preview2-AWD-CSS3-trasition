fcf520ea17531dd690a7b88f80b2602bc516767b
bftsai <tsai19911026@gmail.com>
2023-06-16 02:01:12 +0800

    initialize

---
+++ js/all.js
@@ -0,0 +1,21 @@
+$(document).ready(function () {
+    /*
+    $('.sheet').click(function (e) { 
+        e.preventDefault();
+        $('.compare').toggleClass('active');
+    });
+    $('.button-message').click(function (e) { 
+        e.preventDefault();
+        
+        $('.button-textarea').toggleClass('button-textarea-active');
+        
+       
+       $('.button-textarea').slideToggle(100000);
+       
+    });
+    $('.button-profile').click(function (e) { 
+        e.preventDefault();
+        $('.button-enter-profile').toggleClass('button-enter-profile-active');
+    });
+    */
+});
